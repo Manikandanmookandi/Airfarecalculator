e4bb4e1687e73ed6fa2f627ef99cc994e1fe3dfc
Manikandanmookandi <48524802+Manikandanmookandi@users.noreply.github.com>
2019-03-20 08:00:46 +0000

    Update AirFareApp.java

--- src/main/java/com/afkl/airfare/main/AirFareApp.java
+++ src/main/java/com/afkl/airfare/main/AirFareApp.java
@@ -3,22 +3,26 @@ package com.afkl.airfare.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.afkl.airfare.controller","com.afkl.airfare.authentication","com.afkl.airfare.service"})
 @EnableAsync
-
-public class AirFareApp {
+public class AirFareApp  {
 
 	@Bean("threadPoolTaskExecutor")
-    	public TaskExecutor getAsyncExecutor() {
-        	ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        	executor.setCorePoolSize(20);
-       		executor.setMaxPoolSize(1000);
-        	executor.setWaitForTasksToCompleteOnShutdown(true);
-        	executor.setThreadNamePrefix("Async-");
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
         return executor;
     }
 	
