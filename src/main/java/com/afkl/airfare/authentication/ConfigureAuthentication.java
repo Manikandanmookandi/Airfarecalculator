b1a541abe3904334ed4b921a340d947ff9a0a1c6
Manikandanmookandi <48524802+Manikandanmookandi@users.noreply.github.com>
2019-03-20 07:58:58 +0000

    Update ConfigureAuthentication.java

--- src/main/java/com/afkl/airfare/authentication/ConfigureAuthentication.java
+++ src/main/java/com/afkl/airfare/authentication/ConfigureAuthentication.java
@@ -4,7 +4,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-
+import org.springframework.stereotype.Component;
+@Component
 public class ConfigureAuthentication {
 
 	private static String accessTokenUri="http://localhost:8080/oauth/token";
@@ -14,15 +15,13 @@ public class ConfigureAuthentication {
 	
 	
 	public RestTemplate oAuthRestTemplate() {
-		System.out.println("in configure authentication");
+		
 	    ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();	    
 	    resourceDetails.setGrantType("client_credentials");
 	    resourceDetails.setClientId(oAuth2ClientId);
 	    resourceDetails.setClientSecret(oAuth2ClientSecret);
-	    resourceDetails.setAccessTokenUri(accessTokenUri);
-	    
-	    OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());
-
+	    resourceDetails.setAccessTokenUri(accessTokenUri);	    
+	    OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());	    
 	    return restTemplate;
 	}
 }
